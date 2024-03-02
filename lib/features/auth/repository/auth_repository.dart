d2ea06fa3ded5818d19869ff5342a51ca1534412
fobic <sabychakraborty08@gmail.com>
2024-03-02 23:51:27 +0530

    function(features): made some minor changes to the authentication

---
+++ lib/features/auth/repository/auth_repository.dart
@@ -0,0 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:reddit_clone/core/constants/constants.dart';
+import 'package:reddit_clone/core/constants/firebase_constants.dart';
+import 'package:reddit_clone/core/providers/firebase_providers.dart';
+import 'package:reddit_clone/models/user_model.dart';
+
+final authRepositoryProvider = Provider((ref) => AuthRepository(
+  firestore: ref.read(firestoreProvider), 
+  auth: ref.read(authProvider), 
+  googleSignIn: ref.read(googleSignInProvider), 
+ ),
+);
+
+class AuthRepository {
+  // ignore: unused_field
+  final FirebaseFirestore _firestore;
+  final FirebaseAuth _auth;
+  final GoogleSignIn _googleSignIn;
+
+  AuthRepository({
+    required FirebaseFirestore firestore,
+    required FirebaseAuth auth,
+    required GoogleSignIn googleSignIn,
+  }) : _auth = auth,
+       _firestore = firestore,
+       _googleSignIn = googleSignIn;
+      
+      CollectionReference get _users => _firestore.collection(FirebaseConstants.usersCollection);
+
+  void signInwithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+      // ignore: unused_local_variable
+      final googleAuth = await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: (await googleUser?.authentication)?.accessToken,
+        idToken: (await googleUser?.authentication)?.idToken,
+      );
+
+      UserCredential userCredential = await _auth.signInWithCredential(credential);
+
+      UserModel userModel = UserModel(
+        name: userCredential.user!.displayName??'No Name', 
+        profilePic: userCredential.user!.photoURL??Constants.avatarDefault, 
+        banner: Constants.bannerDefault, 
+        uid: userCredential.user!.uid, 
+        isAuthenticated: true, 
+        karma: 0, 
+        awards: [],
+      );
+
+      await _users.doc(userCredential.user!.uid).set(userModel.toMap());
+    } catch (e) {
+      print(e);
+    }
+  }
+}
